2296080d412c24af209ce0a52cc88db825c2c7b6
Leif Benson <leifbensonbusiness@gmail.com>
2023-07-24 16:48:20 -0600

    basic structure first commit, testing artifacts present for campers

--- Skill Picker Reborn/ContentView.swift
+++ Skill Picker Reborn/ContentView.swift
@@ -7,17 +7,42 @@
 
 import SwiftUI
 
+let campers = [
+    try! Camper(fName: "Joe", lName: "Biden", cabin: "1", preferredSkills: ["Archery","Backcountry","Pelletry","Ultimate","Crafts","Drama"]),
+    try! Camper(fName: "Donald", lName: "Trump", cabin: "2", preferredSkills: ["Ultimate","Pedal Karts","Horses","Canoeing","Hockey",""])
+]
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(){
+            HStack(){
+                Button("Select File"){
+                    
+                }
+                Button("Add Camper"){
+                    
+                }
+                Button("Sort"){
+                    
+                }
+                Button("Export Schedules"){
+                    
+                }
+                .padding([.bottom,.top])
+            }
+            Table(campers){
+                TableColumn("First Name",value: \.fName)
+                TableColumn("Last Name",value: \.lName)
+                TableColumn("Cabin",value: \.cabin)
+                TableColumn("Skill 1",value: \.skillOne)
+                TableColumn("Skill 2",value: \.skillTwo)
+                TableColumn("Skill 3",value: \.skillThree)
+                TableColumn("Skill 4",value: \.skillFour)
+            }
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Skill Picker Reborn/Skill.swift
@@ -0,0 +1,8 @@
+//
+//  Skill.swift
+//  Skill Picker Reborn
+//
+//  Created by Leif Benson on 2023-07-24.
+//
+
+import Foundation
