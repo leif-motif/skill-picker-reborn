4e63dd6c35e5071bb37087c1947d65acddb61bdc
Leif Benson <leifbensonbusiness@gmail.com>
2023-07-24 09:08:31 -0600

    Initial Commit

---
+++ Skill Picker Reborn/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Skill Picker Reborn
+//
+//  Created by Leif Benson on 2023-07-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Skill Picker Reborn/Skill_Picker_RebornApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skill_Picker_RebornApp.swift
+//  Skill Picker Reborn
+//
+//  Created by Leif Benson on 2023-07-24.
+//
+
+import SwiftUI
+
+@main
+struct Skill_Picker_RebornApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
