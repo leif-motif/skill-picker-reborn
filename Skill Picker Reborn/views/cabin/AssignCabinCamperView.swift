0d2f57c79a639200349bd0c7c06d74a47df5be01
leif-motif <140660659+leif-motif@users.noreply.github.com>
2023-08-14 15:25:43 -0600

    assigning camper to cabin reworked, but using placeholder textfield until a combo box is added

--- Skill Picker Reborn/views/cabin/AssignCabinCamperView.swift
+++ Skill Picker Reborn/views/cabin/AssignCabinCamperView.swift
@@ -23,44 +23,39 @@ import SwiftUI
 struct AssignCabinCamperView: View {
     @EnvironmentObject private var data: CampData
     private var targetCabin: String
-    @State private var selectedCamper = UUID()
-    @State private var noneCamperAlert: Bool = false
+    @State private var camperInput = ""
+    @State private var camperIDs: [String:UUID] = [:]
     @Environment(\.dismiss) var dismiss
     var body: some View {
         Form {
-            Picker("Leader:", selection: $selectedCamper){
-                ForEach(0...(data.campers.count-1), id: \.self){
-                    if(data.campers[$0].cabin != targetCabin){
-                        Text(data.campers[$0].fName+" "+data.campers[$0].lName).tag(data.campers[$0].id)
-                    }
-                }
-            }
-            .padding()
+            //PLEASE REPLACE THIS WITH A COMBO BOX!!!!!!!
+            TextField("Camper:", text: $camperInput)
+                .padding(.bottom)
+            //thank you for your time.
             HStack {
                 Spacer()
                 Button("Cancel") {
                     dismiss()
                 }
                 Button("Assign Camper") {
-                    let targetCamper: Camper? = data.campers.first(where: {$0.id == selectedCamper})
-                    if(targetCamper != nil){
-                        assignCamperToCabin(targetCamper: targetCamper!, cabinName: targetCabin, data: data)
-                        dismiss()
-                    } else {
-                        noneCamperAlert.toggle()
-                    }
+                    assignCamperToCabin(targetCamper: data.campers.first(where: {$0.id == camperIDs[camperInput]})!, cabinName: targetCabin, data: data)
+                    dismiss()
                 }
                 .buttonStyle(.borderedProminent)
                 .tint(.blue)
+                .disabled(!camperIDs.keys.contains(camperInput))
             }
-            .padding([.bottom,.trailing])
         }
-        .frame(width: 270, height: 90)
-        .alert(isPresented: $noneCamperAlert) {
-            Alert(title: Text("Error!"),
-                  message: Text("A camper to assign to the cabin must be selected."),
-                  dismissButton: .default(Text("Dismiss")))
-        }
+        .padding()
+        .frame(width: 290, height: 90)
+        .onAppear(perform: {
+            for camper in data.campers.sorted(by: {$0.lName < $1.lName}) {
+                if(camper.cabin != targetCabin){
+                    camperIDs[camper.fName+" "+camper.lName] = camper.id
+                }
+            }
+            print(camperIDs)
+        })
     }
     init(targetCabin: String) {
         self.targetCabin = targetCabin
