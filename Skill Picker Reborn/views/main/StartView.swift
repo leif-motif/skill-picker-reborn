1ee37164c1b7088c27e53ebb03484b91b6387b1b
leif-motif <140660659+leif-motif@users.noreply.github.com>
2023-08-13 20:43:28 -0600

    re-activated tests

--- Skill Picker Reborn/views/main/StartView.swift
+++ Skill Picker Reborn/views/main/StartView.swift
@@ -54,14 +54,14 @@ struct StartView: View {
                 
             }
             .help("View Software License")
-            /*Button {
+            Button {
                 createTestingDataPlus(data: data)
             } label: {
                 Image(systemName: "testtube.2")
                     .foregroundColor(data.campers.count > 0 ? Color(.systemGray) : Color(.systemCyan))
             }
             .help("Create Testing Data")
-            .disabled(data.campers.count > 0)*/
+            .disabled(data.campers.count > 0)
         }
     }
 }
